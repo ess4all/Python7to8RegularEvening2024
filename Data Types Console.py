65f98d58e4abc94291c6bbb168238a7991f89b5a
ess4all <ankitk8285174895@gmail.com>
2024-09-03 19:51:38 +0530

    Add files via upload

---
+++ Data Types Console.py
@@ -0,0 +1,39 @@
+Python 3.12.4 (v3.12.4:8e8a4baf65, Jun  6 2024, 17:33:18) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 5
+type(x)
+<class 'int'>
+x = True
+type(x)
+<class 'bool'>
+x = 0.5
+type(x)
+<class 'float'>
+x = "hello"
+type(x)
+<class 'str'>
+>>> x = 3+4j#j is iota
+>>> x
+(3+4j)
+>>> type(x)
+<class 'complex'>
+>>> 
+= RESTART: /Users/sahilkumar/Desktop/Prakash_Evening_7to8_Sept_Regular_2024/io.py
+32
+>>> 
+= RESTART: /Users/sahilkumar/Desktop/Prakash_Evening_7to8_Sept_Regular_2024/io.py
+The Sum of 12 and 20 is 32
+>>> 
+= RESTART: /Users/sahilkumar/Desktop/Prakash_Evening_7to8_Sept_Regular_2024/io.py
+The Sum of 12 and 20 is 32
+The Sum of 12 and 20 is 32
+>>> 
+= RESTART: /Users/sahilkumar/Desktop/Prakash_Evening_7to8_Sept_Regular_2024/io.py
+The Sum of 12 and 20 is 32
+The Sum of 12 and 20 is 32
+The Sum of {x} and {y} is {x+y}
+
+= RESTART: /Users/sahilkumar/Desktop/Prakash_Evening_7to8_Sept_Regular_2024/io.py
+The Sum of 12 and 20 is 32
+The Sum of 12 and 20 is 32
+The Sum of 12 and 20 is 32
---
+++ io.py
@@ -0,0 +1,6 @@
+#how to take input and print it
+x = int(input("Enter No1:"))
+y = int(input("Enter No2:"))
+#print("The Sum of",x,"and",y,"is",x+y)
+#print("The Sum of {} and {} is {}".format(x,y,x+y))
+print(f"The Sum of {x} and {y} is {x+y}")
